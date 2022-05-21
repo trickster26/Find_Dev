4e946df15db8118f20bf1968a9101785076edd2d
ANU KUMAR <68609057+trickster26@users.noreply.github.com>
2022-05-21 18:52:07 +0530

    Add files via upload

--- Django Learning Projects/projects/forms.py
+++ Django Learning Projects/projects/forms.py
@@ -1,4 +1,7 @@
+from logging import PlaceHolder
+from tkinter import Widget
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 
@@ -6,3 +9,15 @@ class ProjectForm(ModelForm):
     class Meta:
         model=Project 
         fields = ['title','description','demo_link','source_link','tags']
+        widgets ={
+            'tags':forms.CheckboxSelectMultiple(),
+        }  
+        
+    def __init__(self,*args,**kwargs):
+        super(ProjectForm,self).__init__(*args,**kwargs)
+        
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
+        
+        # self.fields['title'].widget.attrs.update({'class':'input' })
+        
