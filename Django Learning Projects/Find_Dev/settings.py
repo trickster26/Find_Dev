bd39bb551a7a441493f593a2765c76de1ef1430e
ANU KUMAR <68609057+trickster26@users.noreply.github.com>
2022-05-27 07:51:16 +0530

    Add files via upload

--- Django Learning Projects/Find_Dev/settings.py
+++ Django Learning Projects/Find_Dev/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from xmlrpc.client import _HostType
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +118,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'find.dev01@gmail.com'
+EMAIL_HOST_PASSWORD = 'Dev@01Find'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
