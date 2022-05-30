c85de1aa3df395442a269741718b83b04a156dca
ANU KUMAR <68609057+trickster26@users.noreply.github.com>
2022-05-30 07:50:08 +0530

    Add files via upload

--- Django Learning Projects/api/views.py
+++ Django Learning Projects/api/views.py
@@ -1,5 +1,10 @@
-from django.http import JsonResponse 
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from .serializers import ProjectSerializer
+from projects.models import Project, Review
 
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         {'GET':'/api/projects'},
@@ -9,4 +14,35 @@ def getRoutes(request):
         {'POST':'/api/users/token'},
         {'POST':'/api/users/token/refresh'},
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+@api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+def getProjects(request):
+    projects = Project.objects.all()
+    serializer = ProjectSerializer(projects,many=True)#serializing one object or many 
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getProject(request,pk):
+    project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(project,many=False)#serializing one object or many 
+    return Response(serializer.data)
+ 
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def projectVote(request, pk):
+    project = Project.objects.get(id=pk)
+    user = request.user.profiles
+    data = request.data
+    
+    review, created = Review.objects.get_or_create(    
+    owner = user,
+    project = project,
+    )
+    
+    review.value = data['value']
+    review.save()
+    project.getVoteCount
+    serializer = ProjectSerializer(project , many=False)
+    return Response(serializer.data)
