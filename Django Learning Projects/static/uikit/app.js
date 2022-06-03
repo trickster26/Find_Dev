eee02275dfc292653261b9d19db177de31c46299
ANU KUMAR <68609057+trickster26@users.noreply.github.com>
2022-06-03 09:58:30 +0530

    Add files via upload

--- Django Learning Projects/static/uikit/app.js
+++ Django Learning Projects/static/uikit/app.js
@@ -3,12 +3,12 @@ document.addEventListener('DOMContentLoaded', function () {
   hljs.highlightAll();
 });
 
+
 let alertWrapper = document.querySelector('.alert')
 let alertClose = document.querySelector('.alert__close')
 
-if(alertWrapper){
-  
-  alertClose.addEventListener('click',() =>
-  alertWrapper.getElementsByClassName.display='none'
+if (alertWrapper) {
+  alertClose.addEventListener('click', () =>
+    alertWrapper.style.display = 'none'
   )
 }
